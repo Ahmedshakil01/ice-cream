7cb28eb82fc85fef3d93a79e7f1b4fe8ae1eb304
shakil ahmed <shakil@aleshatech.com.bd>
2022-08-19 11:42:56 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () =>{
+
+    return(
+        <header>
+            <div className="container">
+                <div>
+                    <img className="" src="" alt="Logo" />
+                </div>
+                <div className="textRight">
+                    <img src="" alt="React" className="" />
+                    <strong>React</strong>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/containers/IceCreamBuilder/IceCreamBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class IceCreamBuilder extends Component{
+  state = {}
+    render() {
+        return(
+             <div>
+             </div>
+        )
+    }
+};
